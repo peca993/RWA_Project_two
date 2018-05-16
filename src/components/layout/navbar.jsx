5e2c9939c10dd069241c57ecd4e54f01eaf6da5b
Petar <peca993@gmail.com>
2018-05-16 18:56:42 +0200

    Routing

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -1,44 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from './home';
+import About from './about';
+import Scores from './scores';
 
 export default class Navbar extends React.Component {
 
 
+    
+
     render() {
         return (
-            <div>
-                <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-                    <a class="navbar-brand" href="#">Expand at md</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <Router>
+                <div>
+                <nav className="navbar navbar-expand-md navbar-light bg-secondary">
+                    <a className="navbar-brand">ReactivePoker</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
 
-                    <div class="collapse navbar-collapse" id="navbarsExample04">
-                        <ul class="navbar-nav mr-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    <div className="collapse navbar-collapse" id="navbarsExample04">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">Home</Link>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/about">About</Link>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link disabled" href="#">Disabled</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                                <div class="dropdown-menu" aria-labelledby="dropdown04">
-                                    <a class="dropdown-item" href="#">Action</a>
-                                    <a class="dropdown-item" href="#">Another action</a>
-                                    <a class="dropdown-item" href="#">Something else here</a>
-                                </div>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/scores">Scores</Link>
                             </li>
                         </ul>
-                        <form class="form-inline my-2 my-md-0">
-                            <input class="form-control" type="text" placeholder="Search" />
-                        </form>
                     </div>
                 </nav>
-            </div>
+  
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/about" component={About} />
+                    <Route exact path="/scores" component={Scores} />
+                    </div>
+            </Router>
         );
     }
 } 
--- src/index.js
+++ src/index.js
@@ -3,13 +3,26 @@ import ReactDOM from 'react-dom';
 import Navbar from './components/layout/navbar';
 import './style/bootstrap';
 import $ from 'jquery';
+import Cards from './components/game/cards';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 
 export default class App extends React.Component {
 
+  constructor(){
+    super();
+
+    
+  
+  }
+
+  
   render(){
     return(
     <div>
+      
       <Navbar/>
+        
     </div>
     );
   }
@@ -18,8 +31,10 @@ export default class App extends React.Component {
   
   // ========================================
   
+  
+
   ReactDOM.render(
     <App />,
     document.getElementById('root')
   );
-  
+
